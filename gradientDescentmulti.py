b86ef4269634b6f12b598815040f88eb900e1c53
kanishkcoe <32832273+kanishkcoe@users.noreply.github.com>
2018-02-17 21:49:58 +0530

    code added

--- gradientDescentmulti.py
+++ gradientDescentmulti.py
@@ -3,6 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plot
 from sklearn import datasets
 
+def errorFunction(b, m, points, target):
+    totalMterms = m * points
+    y = target
+
+    msum = totalMterms.sum()
+
+    error = y - msum
+    return error
 
 def compute_error_for_given_points(b, m, points):
     totalError = 0
@@ -15,50 +23,50 @@ def compute_error_for_given_points(b, m, points):
     return totalError / float(len(points))
 
 
-def step_gradient(current_b, current_m, points, learning_rate):
+def step_gradient(current_b, current_m, points, target, learning_rate):
     b_gradient = 0
-    m_gradient = np.array(0 for i in range(len(points)))
-    N = float(len(points[0]))
+    m_gradient = np.zeros(len(points[0]))
+    N = int(len(points))
 
-    for j in range(0, len(points[0])):
-        for i in range(0, len(points)):
-            y = points[i, len(points[0] - 1)]
-            x = points[i, j]
-            b_gradient += -(2/N) * (y - ((current_m * x) + current_b))
-            m_gradient[j] += -(2/N) * x * (y - ((current_m * x) + current_b))
-        new_b = current_b - (learning_rate * b_gradient)
-        new_m = current_m - (learning_rate * m_gradient)
+    for i in range(0, len(points)):
+        y = target[i]
+        x = points[i]
+        b_gradient += -(2 / N) * errorFunction(current_b, current_m, x, y)
+        m_gradient = np.add(m_gradient, np.multiply((-2 / N) * errorFunction(current_b, current_m, x, y), x))
+    new_b = current_b - (learning_rate * b_gradient)
+    new_m = np.subtract(current_m, np.multiply(learning_rate, m_gradient))
     return (new_b, new_m)
 
 
-def gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations):
+def gradient_descent_runner(points, target, initial_b, initial_m, learning_rate, num_iterations):
     b = initial_b
     m = initial_m
 
     for i in range(num_iterations):
-        b, m = step_gradient(b, m, np.array(points), learning_rate)
+        b, m = step_gradient(b, m, points, target, learning_rate)
     return b, m
 
 
 def run():
-    diabetes = datasets.load_diabetes()
+    boston = datasets.load_boston()
     # points to be entered
-    df = pd.DataFrame(diabetes.data)
+    df = pd.DataFrame(boston.data)
+    df = np.array(df.values)
+    target = boston.target
 
     # hyperparameters
     learning_rate = 0.0001
 
     # linear regression formula
     initial_b = 0
-    initial_m = np.array(0 for i in range(len(points)))
+    initial_m = np.zeros(len(df[0]))
+    print("intial m = ", initial_m)
 
-    num_iterations = 1000
+    num_iterations = 150
 
-    b, m = gradient_descent_runner(df, initial_b, initial_m, learning_rate)
+    b, m = gradient_descent_runner(df, target, initial_b, initial_m, learning_rate, num_iterations)
     print("intercept = ", b)
-    print("slopes : ")
-    for i in range(10):
-        print("m", i, " = ", m[i])
+    print("slopes : ", m)
 
 
 if __name__ == '__main__':
